94aeead5b6845b49ca5dd89c85e905ae240fd208
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2023-01-19 20:32:36 -0800

    firebase configured, initialized, db data pulled in real-time to display components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  collection,
+  query,
+  orderBy,
+  limit,
+  getFirestore,
+} from "firebase/firestore";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtwh4x07sKUGYhP6nthW6pNc9BvS8PhkU",
+  authDomain: "chatapp-c4dee.firebaseapp.com",
+  projectId: "chatapp-c4dee",
+  storageBucket: "chatapp-c4dee.appspot.com",
+  messagingSenderId: "11364178911",
+  appId: "1:11364178911:web:8f6d22ced3814d531f426a",
+  measurementId: "G-MKL4BNKRGD",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const firestore = getFirestore(app);
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <SignOut />
       </header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
+}
+
+function ChatMessage(props) {
+  const { text, uid } = props.message;
+
+  return <p>{text}</p>;
+}
+
+function ChatRoom() {
+  const messagesRef = collection(firestore, "messages");
+  const q = query(messagesRef, orderBy("createdAt"), limit(25));
+  const [messages] = useCollectionData(q, { idField: "id" });
+
+  return (
+    <>
+      <div>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+    </>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+
+  return <button onClick={() => signInWithGoogle()}>Sign In</button>;
+}
+
+function SignOut() {
+  return auth.user && <button onClick={() => auth.signOut()}>Sign Out</button>;
 }
 
 export default App;
